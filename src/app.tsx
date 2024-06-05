813a5dfbd45974464f4fbffa5fe9d0a4cdd12378
bayalag.t <bayalag.t@miat.com>
2024-06-05 17:20:53 +0800

    done sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import JobPage from './pages/job-page';
 import CompanyPage from './pages/company-pages';
 import { useState } from 'react';
-import Navbar from './pages/components/navbar';
+// import Navbar from './pages/components/navbar';
 import CreateJobPage from './pages/create-job-page';
 import LoginPage from './pages/login-page';
 import EditJobPage from './pages/edit-job-page.tsx';
@@ -11,7 +11,18 @@ import { getLoggedUserFromToken } from './pages/lib/authentication';
 import { ApolloProvider } from '@apollo/client';
 import { clientApollo } from './graphql/query';
 import '../src/assets/styles/app.scss';
-import PrivateRoute from './pages/components/private-route.tsx'; // Import the PrivateRoute component
+import PrivateRoute from './pages/components/private-route.tsx';
+import Sidebar, { SidebarItem } from './pages/components/Sidebar.tsx';
+import {
+    LayoutDashboard,
+    Home,
+    StickyNote,
+    Layers,
+    Flag,
+    Calendar,
+    LifeBuoy,
+    Settings,
+} from 'lucide-react';
 
 type User = {
     companyId: string;
@@ -25,9 +36,36 @@ function App() {
 
     return (
         <ApolloProvider client={clientApollo}>
-            {loggedUser && (
-                <Navbar loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
-            )}
+            {/*{loggedUser && (*/}
+            {/*    <Navbar loggedUser={loggedUser} setLoggedUser={setLoggedUser} />*/}
+            {/*)}*/}
+            <div className="flex">
+                <Sidebar children={undefined}>
+                    <SidebarItem icon={<Home size={20} />} text="Home" alert />
+                    <SidebarItem
+                        icon={<LayoutDashboard size={20} />}
+                        text="Dashboard"
+                        active
+                    />
+                    <SidebarItem
+                        icon={<StickyNote size={20} />}
+                        text="Projects"
+                        alert
+                    />
+                    <SidebarItem
+                        icon={<Calendar size={20} />}
+                        text="Calendar"
+                    />
+                    <SidebarItem icon={<Layers size={20} />} text="Tasks" />
+                    <SidebarItem icon={<Flag size={20} />} text="Reporting" />
+                    <hr className="my-3" />
+                    <SidebarItem
+                        icon={<Settings size={20} />}
+                        text="Settings"
+                    />
+                    <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+                </Sidebar>
+            </div>
             <main>
                 <Routes>
                     <Route
